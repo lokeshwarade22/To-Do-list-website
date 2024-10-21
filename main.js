d16f78ac8964426ce872072f689dc45e540bfd8b
Lokesh_22 <119560464+lokeshwarade22@users.noreply.github.com>
2024-10-21 22:29:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,74 +1,17 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
-const completedCounter = document.getElementById("completed-counter");
-const uncompletedCounter = document.getElementById("uncompleted-counter");
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
 
-function updateCounters() {
-  const completedTasks = document.querySelectorAll(".completed").length;
-  const uncompletedTasks = document.querySelectorAll("li:not(.completed)").length;
-
-  completedCounter.textContent = completedTasks;
-  uncompletedCounter.textContent = uncompletedTasks;
-}
-
-function addTask() {
-  const task = inputBox.value.trim();
-  if (!task) {
-    alert("Please write down a task");
-    console.log("no task added");
-
-    return;
-  }
-
-  const li = document.createElement("li");
-  li.innerHTML = `
-    <label>
-      <input type="checkbox">
-      <span>${task}</span>
-    </label>
-    <span class="edit-btn">Edit</span>
-    <span class="delete-btn">Delete</span>
-    `;
-
-  listContainer.appendChild(li);
-
-  // clear the input field
-  inputBox.value = " ";
-
-  // attach event listeners to the new task
-  const checkbox = li.querySelector("input");
-  const editBtn = li.querySelector(".edit-btn");
-  const taskSpan = li.querySelector("span");
-  const deleteBtn = li.querySelector(".delete-btn");
-
-  // strike out the completed task
-  checkbox.addEventListener("click", function () {
-    li.classList.toggle("completed", checkbox.checked);
-    updateCounters();
-  });
-
-  editBtn.addEventListener("click", function () {
-    const update = prompt("Edit task:", taskSpan.textContent);
-    if (update !== null) {
-      taskSpan.textContent = update;
-      li.classList.remove("completed");
-      checkbox.checked = false;
-      updateCounters();
+function Add(){
+    if(inputs.value == ""){
+        alert("Please Enter Task")
+    }else{
+        let newEle = document.createElement("ul");
+        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+        inputs.value="";
+        newEle.querySelector("i").addEventListener("click" , remove);
+        function remove(){
+            newEle.remove()
+        }
     }
-  });
-
-  deleteBtn.addEventListener("click", function () {
-    if (confirm("Are you sure you want to delete this task?")) {
-      li.remove();
-      updateCounters();
-    }
-  });
-  updateCounters();
 }
-
-// add task when pressing Enter key
-inputBox.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    addTask();
-  }
-});
